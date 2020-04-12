3d8628b7520bd70d94ea135da761785c2712c756
Lovro <pamet.pijetlova@gmail.com>
2020-04-12 12:00:55 +0200

    Mockito basics

--- app/src/test/java/com/foi_bois/zisprojekt/LoginActivityPresenterTest.java
+++ app/src/test/java/com/foi_bois/zisprojekt/LoginActivityPresenterTest.java
@@ -2,6 +2,10 @@ package com.foi_bois.zisprojekt;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import com.foi_bois.zisprojekt.presenter.LoginActivityPresenter;
 import com.foi_bois.zisprojekt.repositories.UserRepository;
@@ -10,43 +14,58 @@ import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class LoginActivityPresenterTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    LoginActivityView view;
 
     @Test
     public void shouldPassUserToView(){
         //given
-        LoginActivityView view = new MockView();
-        UserRepository userRepo = new UserRepository() {
+        //LoginActivityView view = new MockView();
+        /*UserRepository userRepo = new UserRepository() {
             @Override
             public User getCurrentUser() {
                 return new User("123", "234");
             }
-        };
+        }; */
+
+        User currUser = new User("123", "234");
+        Mockito.when(userRepository.getCurrentUser()).thenReturn(currUser);
 
         //when
-        LoginActivityPresenter presenter = new LoginActivityPresenter(view, userRepo);
+        LoginActivityPresenter presenter = new LoginActivityPresenter(view, userRepository);
         presenter.getCurrentUser();
 
         //then
-        Assert.assertEquals(true, ((MockView) view).displayCurrentUser);
+        //Assert.assertEquals(true, ((MockView) view).displayCurrentUser);
+        Mockito.verify(view).greetUser(currUser);
     }
 
     @Test
     public void shouldHandleWrongPassword(){
-        LoginActivityView view = new MockView();
-        UserRepository userRepo = new UserRepository() {
+       // LoginActivityView view = new MockView();
+     /*   UserRepository userRepo = new UserRepository() {
             @Override
             public User getCurrentUser() {
                 return null;
             }
-        };
+        }; */
 
-        LoginActivityPresenter presenter = new LoginActivityPresenter(view, userRepo);
+        Mockito.when(userRepository.getCurrentUser()).thenReturn(null);
+        LoginActivityPresenter presenter = new LoginActivityPresenter(view, userRepository);
         presenter.getCurrentUser();
 
-        Assert.assertEquals(true, ((MockView) view).displayNoUser);
+        //Assert.assertEquals(true, ((MockView) view).displayNoUser);
+        Mockito.verify(view).displayWrongPasswordError(null);
     }
 
+    //GONE :0
+    /*
     private class MockView implements LoginActivityView {
         boolean displayCurrentUser; //programatically test!
         boolean displayNoUser;
@@ -59,7 +78,7 @@ public class LoginActivityPresenterTest {
         @Override public void displayWrongPasswordError(User currentUser){
             displayNoUser = true;
         }
-    }
+    }*/
 
    /* private class MockUserRepository implements UserRepository {
 
